3ca0e692219271d829fa420852c495b2e05eddac
SergeyCherepanov <sergey@cherepanov.org.ua>
2015-04-24 05:02:39 +0400

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DIR=$(dirname $(readlink -f $0))
-${DIR}/orocrm-root/build.sh
-${DIR}/orocrm-base/build.sh
-${DIR}/orocrm-http/build.sh
+if ${DIR}/orocrm-root/build.sh; then
+  if ${DIR}/orocrm-base/build.sh; then
+    ${DIR}/orocrm-http/build.sh
+  fi
+fi
