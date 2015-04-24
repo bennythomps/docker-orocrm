9128dba3b279c0bc40352d51836a9a4d37e9c413
SergeyCherepanov <sergey@cherepanov.org.ua>
2015-04-24 04:28:00 +0400

    update

--- orocrm-base/bin/install.sh
+++ orocrm-base/bin/install.sh
@@ -7,15 +7,12 @@ function as_user {
     sudo -u ${USER} $@
 }
 
-mkdir -p ${APP_ROOT}
-chown ${USER} ${APP_ROOT}
-
 # download OroCRM
 as_user wget -O /tmp/crm-application-1.6.1.tar.gz -N -P /tmp https://github.com/orocrm/crm-application/archive/1.6.1.tar.gz
 as_user tar -zxf /tmp/crm-application-1.6.1.tar.gz -C /tmp
 ls -al /tmp
-as_user mv /tmp/crm-application-1.6.1 ${APP_ROOT}
-as_user rm /tmp/crm-application-1.6.1.tar.gz
+mv /tmp/crm-application-1.6.1 ${APP_ROOT}
+rm /tmp/crm-application-1.6.1.tar.gz
 as_user composer install -d ${APP_ROOT}
 as_user mv /tmp/parameters.yml ${APP_ROOT}/app/config/parameters.yml
 as_user mv /tmp/OroRequirements.php ${APP_ROOT}/app/OroRequirements.php
