e72832bbf24b2bd8597145040e693c020b902715
SergeyCherepanov <sergey@cherepanov.org.ua>
2015-04-24 04:05:55 +0400

    update

--- orocrm-root/bin/install.sh
+++ orocrm-root/bin/install.sh
@@ -6,7 +6,8 @@ apt-get -y upgrade
 # install base packages
 apt-get install -q -y vim sudo wget curl php5-cli php5-dev \
 php5-mysql php5-curl php5-gd php5-mcrypt php5-sqlite php5-xmlrpc \
-php5-xsl php5-common php5-intl php5-cli php-apc git mcrypt
+php5-xsl php5-common php5-intl php5-cli php-apc git mcrypt \
+python-setuptools
 
 # configure php
 sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini
